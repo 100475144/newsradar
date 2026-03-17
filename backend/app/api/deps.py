33d9e086f89e459f79c5ab4686cab5ee24eaa230
adrian-stefan-giurca <100495924@alumnos.uc3m.es>
2026-03-17 22:03:32 +0100

    Sprint 0: Conexión a la base de datos mediante get_db(). Definidos engine/session/base globalmente para el proyecto.

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,12 +1,24 @@
 from typing import Generator
+from ..core.database import session_newsradar
+from sqlalchemy.orm import Session
 
 
-def get_db() -> Generator[None, None, None]:
+def get_db() -> Generator[Session, None, None]:
     """
-    Placeholder de dependencia de base de datos.
+    Obtener una sesión a la base de datos con cleanup automático
+    La sesión se cierra tras ejecutar el bloque de código desde 
+    el que se haya hecho la llamada a esta función. 
+
+    Para usar get_db() en un endpoint de fastAPI:  
+    def endpoint(db: Session = Depends(get_db)):
+    
     En Sprint 0.3 / Sprint 1 se conectará a SQLAlchemy Session.
     """
-    yield None
+    db = session_newsradar()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 def get_current_user():
---
+++ backend/app/core/database.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from os import getenv
+
+db_url = getenv("DATABASE_URL")
+# la variable de entorno DATABASE_URL se define en 
+# docker-compose.yaml
+
+if db_url is None:
+    print("[ERROR] DATABASE_URL env variable is not defined")
+    exit(-1)
+
+# Engine es el objeto que gestiona la conexión con la base de datos 
+engine = create_engine(
+    url=db_url, 
+    echo=True,   # Mostrar queries en consola, en producción -> False
+    pool_size=10,
+    max_overflow=20,
+    pool_timeout=30,
+    pool_recycle=1800,
+    pool_pre_ping=True
+)
+
+session_newsradar = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)  # Crear sesión con la base de datos
+
+# Base declarativa a usar 
+class Base(DeclarativeBase):
+    pass
+
