3b5e72ea1762dd82cea7cd3b7edf693ff9118787
adrian-stefan-giurca <100495924@alumnos.uc3m.es>
2026-05-23 18:15:12 +0200

    Add more tests for auth feature of the system

--- backend/app/tests/fixtures/auth.py
+++ backend/app/tests/fixtures/auth.py
@@ -1,4 +1,5 @@
 import pytest
+from app.modules.auth.models import User
 
 @pytest.fixture
 def user_data():
@@ -10,17 +11,23 @@ def user_data():
         "password": "password123"
     }
 
-
 @pytest.fixture
 def registered_user(client, user_data):
     client.post("/api/v1/auth/register", json=user_data)
     return user_data
 
+@pytest.fixture
+def verify_user(registered_user, db):
+    # Verificar usuario directamente en la BD
+    user = db.query(User).filter_by(email=registered_user["email"]).first()
+    user.is_verified = True
+    db.commit()
+    return registered_user
 
 @pytest.fixture
-def auth_token(client, registered_user):
+def auth_token(client, verify_user):
     response = client.post("/api/v1/auth/login", json={
-        "email": registered_user["email"],
-        "password": registered_user["password"]
+        "email": verify_user["email"],
+        "password": verify_user["password"]
     })
     return response.json()["access_token"]
