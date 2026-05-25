47a7048e9419a6e2f1d3f1d348a5693a2036e20d
CristinaLpzAlc <100475144@alumnos.uc3m.es>
2026-05-25 11:30:48 +0200

    hecho 2

--- backend/app/alembic/versions/a7b8c9d0e1f2_add_phone_to_users.py
+++ backend/app/alembic/versions/a7b8c9d0e1f2_add_phone_to_users.py
@@ -16,7 +16,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "a7b8c9d0e1f2"
-down_revision: Union[str, Sequence[str], None] = "f4d5e6f7a8b9"
+down_revision: Union[str, Sequence[str], None] = "f9c0d1e2f3a4"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
